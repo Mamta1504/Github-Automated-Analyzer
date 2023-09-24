751c6d6b6addae09dadbd07aca025b52b0c5b99d
Mamta Kumari <55489031+Mamta1504@users.noreply.github.com>
2023-09-25 03:06:54 +0530

    app.py

--- app.py
+++ app.py
@@ -1,35 +1,69 @@
 import streamlit as st
 from fetch_file import fetch_github_repositories
 
-
+# Function to handle user input
 def user_input():
+    """
+    Display a title and text input field for the GitHub user's URL.
+    
+    Returns:
+        str: The GitHub user's URL entered by the user.
+    """
     st.title('Github Automated Analysis')
-    url = st.text_input("Please Enter The GitHub User's URL")
-    return url
+    github_user_url = st.text_input("Please Enter The GitHub User's URL")
+    return github_user_url
 
+# Function to display introductory information
+def display_intro():
+    """
+    Display introductory information about the application.
+    """
+    # Display the large heading in one complete line
+    st.markdown("<h1 style='text-align: center;'>🚀 Welcome to the GitHub Repository Analyzer! 🚀</h1>", unsafe_allow_html=True)
+    
+    # Add the rest of the project description written here
+    st.write("Are you ready to explore the coding depths of GitHub's brightest minds?")
+    st.write("Our Python-powered utility is your gateway to uncovering the most captivating, technically demanding repositories within any GitHub user's profile.")
+    st.write("🔍 How does it work?")
+    st.write("Just enter the GitHub user's URL in the provided textbox, hit that 'Enter' button, and let the magic begin! Our tool harnesses the power of GPT and LangChain to meticulously assess each repository, singling out the one that boasts the greatest technical prowess.")
+    st.write("📊 What's in it for you?")
+    st.write("Once the analysis is complete, we'll proudly present the crown jewel—the repository that stands as a testament to intricate coding artistry. It's not just about finding code; it's about discovering inspiration, learning, and connecting with the brilliant minds behind these projects.")
+    st.write("🤝 Join the GitHub Community")
+    st.write("Our tool isn't just about repositories; it's about building connections. Use it to strike up conversations, collaborate, and learn from the experts who create these exceptional repositories. The GitHub community is waiting for you to dive in and make your mark.")
+    st.write("Ready to embark on a journey of technical discovery? Get started now and unlock the world of GitHub's most exceptional repositories!")
+
+
+# Function to display the result
+def display_result(github_user_url, most_complex_repository_name, most_complex_repository_link):
+    """
+    Display the result of the analysis.
+
+    Args:
+        github_user_url (str): The URL of the user's GitHub profile.
+        most_complex_repository_name (str): The name of the most complex repository.
+        most_complex_repository_link (str): The URL of the most complex repository.
+    """
+    if most_complex_repository_name and most_complex_repository_link:
+        st.write(f"The most technically complex repository is **'{most_complex_repository_name}'**.")
+        # Display "Link" as clickable text that redirects to the repository
+        st.write(f"Here is the direct link to the repository: [Link]({most_complex_repository_link})")
+    else:
+        st.write("Ohh!!! Some Error Encountered")
+
+# Main function
 def main():
-    st.title("Welcome to the GitHub Repository Analyzer")   
-    st.write("""This Python-based utility is designed with the purpose of assisting you in identifying the most technically intricate and demanding repository within a specified GitHub user's profile. Simply input the GitHub user's URL in the provided textbox and initiate the process by clicking "Enter".""")
+    """
+    The main function that coordinates the application.
+    """
+    # Display introductory information
+    display_intro()
+    
+    # Get user input
+    github_user_url = user_input()
 
-    st.write("""Upon submitting the URL, our tool utilizes GPT and LangChain to individually evaluate each repository, ultimately pinpointing the one that poses the greatest technical challenge.""")
-             
-    st.write("""Once all repositories have been analyzed, the tool will conclusively determine and present the most technically demanding repository. This offers valuable insights into the user's most intricate and advanced projects.""")
-             
-    st.write("""We trust that this tool will aid you in discovering exceptional repositories and establishing connections with highly skilled developers in the GitHub community.""")
-
-    st.write("Don't hesitate—get started now! Enjoy your exploration!")
-
-
-    github_url = user_input()
-
-    if github_url:
-        most_complex_repository_name, most_complex_repository_link = fetch_github_repositories(github_url)
-
-        if most_complex_repository_name and most_complex_repository_link:
-            st.write(f"The most technically complex repository from user '{github_url}' is '{most_complex_repository_name}'.\n")
-            st.write(f"Here is the direct link to the repository: [{most_complex_repository_name}]({most_complex_repository_link})")
-        else:
-            st.write("Ohh!!! Some Error Encountered")
+    if github_user_url:
+        most_complex_repository_name, most_complex_repository_link = fetch_github_repositories(github_user_url)
+        display_result(github_user_url, most_complex_repository_name, most_complex_repository_link)
 
 if __name__ == "__main__":
     main()
